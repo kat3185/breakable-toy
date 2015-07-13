ba587f7eb05e677a3c8388109bdd3dfc2f516eef
kat3185 <Ken.Thomas.3185@gmail.com>
2015-07-13 17:07:37 -0400

    tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  devise_for :users
+  devise_for :users,
+             :controllers => { :registrations => "my_devise/registrations" }
 
   resources :courses, only: [:index, :show]
   resources :course_registrations, only: [:destroy]
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
+  let(:student) { FactoryGirl.create(:student) }
+
+  it 'has a working factory' do
+    expect(student.first_name).to be_a(String)
+    expect(student.last_name).to be_a(String)
+    expect(student.email).to be_a(String)
+  end
 
   it "#full_name" do
-    student = FactoryGirl.create(:student)
     expect(student.full_name).to eq("#{student.first_name} #{student.last_name}")
   end
-  
+
   it "#assign_user(current_user)" do
-    student = FactoryGirl.create(:student)
     user = FactoryGirl.create(:user)
-    student.user = user
-    user.student = student
+    student.assign_user(user)
     expect(student.user.email).to eq(user.email)
     expect(user.student.first_name).to eq(student.first_name)
   end
