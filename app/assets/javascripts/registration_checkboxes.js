667f8038420a3f7a42215c47eed3f11e986b36ac
kat3185 <Ken.Thomas.3185@gmail.com>
2015-07-20 15:43:57 -0400

    houndagain

--- app/assets/javascripts/registration_checkboxes.js
+++ app/assets/javascripts/registration_checkboxes.js
@@ -3,11 +3,11 @@ var e = document.getElementById("student_discount_current_student");
 $('#new_student :checkbox').change(function() {
   var discount = e.options[e.selectedIndex].text;
   if (discount == "No") {
-    var singleClass = "50"
-    var doubleClass = "90"
+    var singleClass = "50";
+    var doubleClass = "90";
   } else if (discount == "Yes") {
-    var singleClass = "40"
-    var doubleClass = "70"
+    var singleClass = "40";
+    var doubleClass = "70";
   }
   var checkCount = $("[type='checkbox']:checked").length;
   if (checkCount < 1){
@@ -22,14 +22,14 @@ $('#new_student :checkbox').change(function() {
 $('#new_student #student_discount_current_student').change(function() {
   var discount = e.options[e.selectedIndex].text;
   if (discount == "No") {
-    var singleClass = "50"
-    var doubleClass = "90"
+    var singleClass = "50";
+    var doubleClass = "90";
   } else if (discount == "Yes") {
-    var singleClass = "40"
-    var doubleClass = "70"
+    var singleClass = "40";
+    var doubleClass = "70";
   }
   var checkCount = $("[type='checkbox']:checked").length;
-  if (checkCount < 1){
+  if (checkCount < 1) {
     $( "span.running-total" ).text("0");
   } else if (checkCount == 1){
     $( "span.running-total" ).text(singleClass);
