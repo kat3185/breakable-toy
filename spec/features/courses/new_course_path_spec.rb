d3a402a74fbde4e3d31d4580cd7b048e03541edb
kat3185 <Ken.Thomas.3185@gmail.com>
2015-07-24 11:15:15 -0400

    dates and test

--- spec/features/courses/new_course_path_spec.rb
+++ spec/features/courses/new_course_path_spec.rb
@@ -21,13 +21,29 @@ feature 'create a new course', %Q{
     expect(page).to have_no_button("Submit Course")
   end
 
-  scenario "admin visits the new courses page" do
-    user = FactoryGirl.create(:user, :admin)
-    sign_in_as(user)
+  scenario "admin visits the new courses page", js: true do
+    admin = FactoryGirl.create(:user, :admin)
+    sign_in_as(admin)
+    FactoryGirl.create(:venue)
+    FactoryGirl.create_list(:instructor, 2)
+    FactoryGirl.create(:meeting_date)
 
     visit new_course_path
+
+    expect(Course.all.count).to eq(0)
     expect(page).to have_no_content("You must be logged in as an administrator to create a class!")
     expect(page).to have_button("Submit Course")
+
+    fill_in "Title", with: "Lindy Class"
+    fill_in "Description", with: "learn stuff!"
+    fill_in "Time", with: "730-830 PM"
+    fill_in "Body", with: "lorum ipsum claaaaaaaaaass"
+    all("input[type=checkbox]").each(&:click)
+    page.execute_script("$('#course_meeting_date__first').val('08/08/2015')")
+    click_button("Submit Course")
+
+    expect(Course.all.count).to eq(1)
+
   end
 
 end
